59b296a2ec9a74dbb3c161ae7c2f06d19c802405
Richard May <mybrainishuge@users.noreply.github.com>
2016-05-26 12:31:13 -0700

    Create package.json

--- db/db-config.js
+++ db/db-config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const knex = require('knex')({
-  client: 'postgres???',
-  connection: {},
+  client: 'postgresql',
+  connection: {
+    database: 'hackdin'
+  },
 });
 
 const db = require('bookshelf')(knex);
