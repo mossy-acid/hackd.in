2d69851817d72b0598971de3000c6597ec4d4f07
Richard May <mybrainishuge@users.noreply.github.com>
2016-05-26 15:53:40 -0700

    Connect postgresql db

--- server/routes.js
+++ server/routes.js
@@ -29,7 +29,119 @@ module.exports = server => {
   // server.get('/engineers', 'list all engineers');
 
   server.post('/signup', 'submit new user signup');
-  server.post('/create', 'submit new project');
+
+
+/*   */
+
+  app.post('/create', utils.validateUser,
+  function(req, res) {
+    var uri = req.body.url;
+
+    if (!util.isValidUrl(uri)) {
+      console.log('Not a valid url: ', uri);
+      return res.sendStatus(404);
+    }
+
+    new Link({ url: uri }).fetch().then(function(found) {
+      if (found) {
+        res.status(200).send(found.attributes);
+      } else {
+        util.getUrlTitle(uri, function(err, title) {
+          if (err) {
+            console.log('Error reading URL heading: ', err);
+            return res.sendStatus(404);
+          }
+
+          Links.create({
+            url: uri,
+            title: title,
+            baseUrl: req.headers.origin
+          })
+          .then(function(newLink) {
+            res.status(200).send(newLink);
+          });
+        });
+      }
+    });
+  });
+
+  app.post('/login',
+  function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    new User({ username: username }).fetch().then(function(user) {
+      if (user) {
+        bcrypt.compare(password, user.get('password'), function(err, match) {
+          if (match) {
+            console.log('Logging in...');
+            req.session.username = username;
+            res.status(200);
+            res.redirect('/');
+          } else {
+            console.log('Invalid password');
+            res.redirect('/login');
+          }
+        });
+      } else {
+        res.status(200);
+        res.redirect('/login');
+      }
+    });
+  });
+
+
+  app.post('/signup',
+  function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    new User({ username: username }).fetch().then(function(found) {
+      if (found) {
+        res.status(200);
+        res.redirect('/signup');
+      } else {
+        bcrypt.hash(req.body.password, null, null, function(err, hash) {
+          if (err) {
+            console.log('BCRYPT HASH ERROR:', err);
+            res.status(200);
+            res.redirect('/signup');
+          } else {
+            Users.create({
+              username: username,
+              password: hash
+            })
+            .then(function(user) {
+              req.session.username = username;
+              res.status(200);
+              res.redirect('/');
+            });
+          }
+        });
+      }
+    });
+  });
+
+  app.get('/*', function(req, res) {
+    new Link({ code: req.params[0] }).fetch().then(function(link) {
+      if (!link) {
+        res.redirect('/');
+      } else {
+        var click = new Click({
+          linkId: link.get('id')
+        });
+
+        click.save().then(function() {
+          link.set('visits', link.get('visits') + 1);
+          link.save().then(function() {
+            return res.redirect(link.get('url'));
+          });
+        });
+      }
+    });
+  });
+
+
 
 
 };
--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 // const bcrypt = require('bcrypt-nodejs');
 
 const server = express();
-var port = process.env.PORT || 3000;
+const db = require('../db/db-config.js');
+let port = process.env.PORT || 3000;
 
 server.set('view engine', 'ejs');
 
 server.use(express.static(__dirname + '../client'));
 
-server.use(session({
-  secret: 'this is not a secret',
-  cookie: { maxAge: 60000 }
-}));
+// server.use(session({
+//   secret: 'this is not a secret',
+//   cookie: { maxAge: 60000 }
+// }));
 
 server.listen(port);
 module.exports = server;
