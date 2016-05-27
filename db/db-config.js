6943f75f0182329331d4c98386b3e0972577f50e
Kamal Mango <kamal.mango7@gmail.com>
2016-05-27 12:15:00 -0700

    add technologies and ProjectTechnologies tables

--- db/db-config.js
+++ db/db-config.js
@@ -14,8 +14,8 @@ db.knex.schema.hasTable('projects').then(exists => {
       project.increments('id').primary();
       project.string('title').unique();
       project.string('description');
-      project.string('technologies');
-      // project.integer('school_id').unsigned().inTable('schools').references('id');
+      project.integer('technology_id').unsinged.references('id').inTable('technologies');
+      project.integer('school_id').unsigned().references('id').inTable('schools');
       // project.integer('visits');
       // project.timestamps();
     }).then(table => {
@@ -35,8 +35,8 @@ db.knex.schema.hasTable('engineers').then(exists => {
       engineer.string('bio');
       // engineer.string('github');
       // engineer.string('linkedin');
-      // engineer.integer('project_id').unsigned().inTable('projects').references('id');
-      // engineer.integer('school_id').unsigned().inTable('schools').references('id');
+      engineer.integer('project_id').unsigned().references('id').inTable('projects');
+      engineer.integer('school_id').unsigned().references('id').inTable('schools');
       // engineer.integer('visits');
       // engineer.timestamps();
     }).then(table => {
@@ -58,4 +58,34 @@ db.knex.schema.hasTable('schools').then(exists => {
   }
 });
 
+db.knex.schema.hasTable('technologies').then(exists => {
+  if (!exists) {
+    db.knex.schema.createTable('technologies', technology => {
+      technology.increments('id').primary();
+      technology.string('name').unique();
+    }).then(table => {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+// Join table
+db.knex.schema.hasTable('ProjectsTechnologies').then(exists => {
+  if (!exists) {
+    db.knex.schema.createTable('ProjectsTechnologies', projTech => {
+      projTech.increments('id').primary();
+      projTech.integer('project_id');
+      projTech.integer('technology_id');
+    }).then(table => {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+
 module.exports = db;
+
+
+
+
+
