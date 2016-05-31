a8c8b7daf02e71ec70de424f618c0765232285bd
Justin Lai <justin.th.lai@gmail.com>
2016-05-31 01:43:30 -0700

    changed bio edit to text area

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -18,7 +18,14 @@ class Profile extends React.Component {
   }
 
   renderField(field) {
-    if (this.state.edit[field]) {
+    if (this.state.edit[field] && field === 'bio') {
+      return (
+        <div>
+          <textarea id={field+'Edit'} placeholder={'Enter new '+field}></textarea>
+          <button type='button' className={field+' glyphicon glyphicon-edit'} onClick={this.clickEdit}>Save</button>
+        </div>
+      )
+    } else if (this.state.edit[field]) {
       return (
         <div>
           <input id={field+'Edit'} placeholder={'Enter new '+field}></input>
@@ -53,7 +60,7 @@ class Profile extends React.Component {
 
   render() {
     return (
-        <div className='actual-content profile-container'>
+      <div className='actual-content profile-container'>
         <div className="screenshot">
           <img src='https://octodex.github.com/images/codercat.jpg'/>
         </div>
@@ -73,42 +80,9 @@ class Profile extends React.Component {
 
           {this.renderField('github')}
         </div>
-      </div>
+    </div>
     )
   }
 }
 
-//   render() {
-//     return (
-//       <div className="actual-content">
-//         <div id="form-input">
-//           <form className="form" id="form1">
-//             <p className="projectTitle">
-//               <input name="projectTitle" type="text" className="formInput" placeholder="Project Title" id="projectTitle" />
-//             </p>
-//             <p className="contributors">
-//               <input name="contributors" type="text" className="formInput" id="contributors" placeholder="Contributors" />
-//             </p>
-//             <p className="technologies">
-//               <input name="technologies" type="text" className="formInput" id="technologies" placeholder="Technologies" />
-//             </p>
-
-//             <p className="image">
-//               <input name="image" type="text" className="formInput" id="image" placeholder="Image" />
-//             </p>
-
-//             <p className="projectDescription">
-//               <textarea name="projectDescription" className="formInput" id="projectDescription" placeholder="Project Description"></textarea>
-//             </p>
-//           </form>
-//           <div className="submit">
-//             <input type="button" value="SUBMIT" onClick={this.clickHandler} id="button-blue"/>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App
 window.Profile = Profile;
