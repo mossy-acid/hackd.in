9181e53391bba4b10f4b451bc7703c400e4bc577
Richard May <mybrainishuge@users.noreply.github.com>
2016-06-01 15:10:15 -0700

    Add helmet security package

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const express    = require('express');
 const session    = require('express-session');
 const db         = require('../db/db-config.js');
 const bodyParser = require('body-parser');
+const helmet     = require('helmet');
 
 const server = express();
 
+server.use(helmet());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 server.use(session({
   secret: 'worst kept secret',
+  name: 'sessionId',
   resave: true,
   saveUninitialized: true
 }));
